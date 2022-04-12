351d3d7e696fc98f814537c8ca73c36ab8903859
Meri <mgogi13@freeuni.edu.ge>
2022-04-12 15:12:52 +0400

    Add routes to the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :foods, only: [:index, :create, :destroy]
+  resources :recipes, only: [:index, :show, :create, :destroy]
+  get '/public_recipes/', to: 'public_recipes#index'
+  get '/shopping_list/', to: 'shopping_list#index'
 end
