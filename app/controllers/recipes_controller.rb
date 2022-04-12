465d720200bae8c17ac5778a3b2b312287fb249d
Meri <mgogi13@freeuni.edu.ge>
2022-04-12 18:42:17 +0400

    Add actions to the recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,35 @@
 class RecipesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @user = @recipe.author
+  end
+
+  def create
+    recipe = current_user.recipes.new(recipe_params)
+    if recipe.save
+      flash[:notice] = 'Recipe was created successfully.'
+      redirect_to
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:recipe_id])
+    flash[:notice] = 'Recipe was deleted successfully.'
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
