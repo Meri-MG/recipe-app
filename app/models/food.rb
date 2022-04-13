a5fbb987d355e7162f209d10e92daffd9c9203fe
Meri <mgogi13@freeuni.edu.ge>
2022-04-13 14:15:29 +0400

    Add validations to models

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,8 @@ class Food < ApplicationRecord
   has_many :recipes, through: :recipe_foods, dependent: :destroy
   has_many :inventory_foods, foreign_key: 'food_id', dependent: :destroy
   has_many :inventories, through: :inventory_foods, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :measurement_unit, presence: true, inclusion: { in: %w(grams units), message: "%{value} is not a measurement unit" } 
+  validates :price, presence: true, comparison: { greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :recipes, foreign_key: 'user_id', dependent: :destroy
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
   has_many :inventories, foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
 end
