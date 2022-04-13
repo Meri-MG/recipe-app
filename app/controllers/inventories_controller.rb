e0ca4e96987e914579827e070c88b977555c5d7b
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-13 18:59:37 +0200

    Creating controller for CRUD without U of inventories

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,35 @@
 class InventoriesController < ApplicationController
   def index
+    @inventories = current_user.inventories
   end
 
   def show
+  end
+
+  def destroy
+    current_user.inventories.find(params[:id]).destroy
+    flash[:notice] = 'Inventory was successfully removed'
+    splitted_path = request.path.split('/')
+    splitted_path.pop 
+    redirect_to splitted_path.join('/')
+  end
+
+  def new
+    @new_inventory = Inventory.new
+  end
+
+  def create
+    inventory = Inventory.new(user: current_user, name: params[:inventory][:name])
+    puts "params = #{params}"
+    puts "inventory = #{inventory}"
+    respond_to do |format|
+      if inventory.save
+        flash[:notice] = 'Created an inventory succesfully'
+        format.html { redirect_to '/inventories' }
+      else
+        flash[:notice] = 'Failed to create an inventory. Try again'
+        format.html { redirect_to '/inventories/new' }
+      end
+    end
   end
 end
