40c98dbf7ccdb2646206b69ef58d0b7bf403fa66
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-13 17:35:53 +0200

    Generate inventories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'inventories/index'
+  get 'inventories/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
