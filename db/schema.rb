1b8e3eb14af821b112ad93a47126b3a1af13342b
Meri <mgogi13@freeuni.edu.ge>
2022-04-13 14:00:52 +0400

    Add default value to public column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_091715) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_13_095921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_091715) do
     t.decimal "preparation_time"
     t.decimal "cooking_time"
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
