3411d7d3596f263e5a3c6364443944fae895fe4a
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-14 22:25:49 +0200

    Finish modal window in recipe  details

--- app/assets/javascript/modal-window.js
+++ app/assets/javascript/modal-window.js
@@ -1,4 +1,21 @@
+var modal = document.getElementById("myModal");
 
-let openAlert = () => {
-  alert('This is alert AAAAAAA');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+let openModal = () => {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
