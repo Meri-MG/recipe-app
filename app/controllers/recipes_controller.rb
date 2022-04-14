94ace9ff7a3814d340e252d8d81540d8e5319890
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-15 01:08:57 +0200

    Create recipe controller request specs

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
 class RecipesController < ApplicationController
   def index
-    # @user = User.find(params[:user_id])
     @recipes = current_user.recipes
   end
 
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'testing request' do
+    it "GET /recipes" do
+      user = User.create!(name: 'Dan')
+      get('/recipes')
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "GET /recipes/:id" do
+      user = User.create!(name: 'Dan')
+      recipe = user.recipes.create!(name: 'Chopped apples', preparation_time: 1, cooking_time: 2, description: 'In order to cook...')
+      get("/recipes/#{recipe.id}")
+      expect(response).to render_template('show')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "GET /recipes/new" do
+      get('/recipes/new')
+      expect(response).to render_template('new')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "POST /recipes" do
+      user = User.create!(name: 'Amkam')
+      post('/recipes', params: { recipe: { name: 'Recipe #n', preparation_time: 1.0, cooking_time: 2.0, description: 'Blah...', public: '1' } })
+      get("/recipes/#{Recipe.last.id}")
+      expect(Recipe.last.name).to eq 'Recipe #n'
+      expect(response).to render_template('show')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "DELETE /recipes/:id" do
+      user = User.create!(name: 'Amkam')
+      recipe = user.recipes.create!(name: 'Chopped apples', preparation_time: 1, cooking_time: 2, description: 'In order to cook...')
+      delete('/recipes/' + recipe.id.to_s)
+      expect {
+        get('/recipes/' + recipe.id.to_s)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+      expect(response).to_not render_template('show')
+      expect(response).to_not have_http_status(:ok)
+    end
   end
 end
