4a715eb18c9e6cea08fd43fa005a1b750af12d8c
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-15 00:38:18 +0200

    Generate rspec for inventories controller

--- spec/requests/inventories_spec.rb
+++ spec/requests/inventories_spec.rb
@@ -1,7 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "Inventories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'testing request' do
+    it "GET /inventories" do
+      user = User.create!(name: 'Dan')
+      get('/inventories')
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(:ok)
+    end
+    
+    it "GET /inventories/:id" do
+      user = User.create!(name: 'Dan')
+      inventory1 = user.inventories.create!(name: 'Inventory #1')
+      inventory2 = user.inventories.create!(name: 'Inventory #2')
+      get('/inventories/' + inventory2.id.to_s)
+      expect(response).to render_template('show')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "GET /inventories/new" do
+      get('/inventories/new')
+      expect(response).to render_template('new')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "POST /inventories" do
+      user = User.create!(name: 'Amkam')
+      post('/inventories', params: { inventory: { name: 'Inventory #n' } })
+      get("/inventories/#{Inventory.last.id}")
+      expect(Inventory.last.name).to eq 'Inventory #n'
+      expect(response).to render_template('show')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "DELETE /inventories/:id" do
+      user = User.create!(name: 'Amkam')
+      inventory = user.inventories.create!(name: 'Inventory #m')
+      delete('/inventories/' + inventory.id.to_s)
+      expect {
+        get('/inventories/' + inventory.id.to_s)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+      expect(response).to_not render_template('show')
+      expect(response).to_not have_http_status(:ok)
+    end
   end
 end
