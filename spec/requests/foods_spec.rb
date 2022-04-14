6c4deb424a0e708fc145060907bca51af4c5489d
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-14 23:49:10 +0200

    Generate rspec helpers & write tests for food controller

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Foods", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'testing request' do
+    it "GET /foods" do
+      get('/foods')
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "GET /foods/:id" do
+      get('/foods/1')
+      expect(response).to render_template('show')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "GET /foods/new" do
+      get('/foods/new')
+      expect(response).to render_template('new')
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
