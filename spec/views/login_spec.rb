80d1f1f1b740960c003d947c709c193aadaae8af
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-15 19:01:11 +0200

    Add tests for log in page

---
+++ spec/views/login_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Static content', type: :system do
+  it 'shows the login page' do
+    visit '/users/sign_in'
+    expect(page).to have_content('Log in')
+  end
+
+  it 'has email, password and login fields in the form' do
+    visit '/users/sign_in'
+    expect(page).to have_field('Email')
+    expect(page).to have_field('Password')
+    expect(page).to have_button('Log in')
+  end
+
+  it 'has Email field' do
+    visit '/users/sign_in'
+    click_button 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  it 'inputs proper credentials' do
+    visit '/users/sign_in'
+    User.create!(name: 'Goodman', email: 'bogdan@example.com', password: 'password', confirmed_at: Time.now)
+    fill_in 'user_email', with: 'bogdan@example.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_current_path(root_path)
+  end
+
+  it 'inputs improper credentials' do
+    visit '/users/sign_in'
+    User.create!(name: 'Goodman', email: 'bogdan@example.com', password: 'password', confirmed_at: Time.now)
+    fill_in 'user_email', with: 'bogdan@example.com'
+    fill_in 'user_password', with: 'passsword'
+    click_button 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+end
