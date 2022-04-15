cb92ac83c775b2626efc1c03b92e5c2176cecd20
Meri <mgogi13@freeuni.edu.ge>
2022-04-15 13:36:55 +0400

    Add model specs for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'User') }
+
+  before { subject.save }
+
+  describe 'Associations' do
+    it { should have_many(:recipes) }
+    it { should have_many(:foods) }
+    it { should have_many(:inventories) }
+  end
+
+  describe 'validations' do 
+    it 'isn\'t valid without name' do
+      subject.name = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'isn\'t valid with name less than 3 characters' do
+      subject.name = 'ha'
+      expect(subject).to_not be_valid
+    end
+  end
+end
