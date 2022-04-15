cd160dd7941a4d6f55e4de5386cbde5d5ba393a7
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-15 10:44:49 +0200

    Create rspec for inventory foods controller

--- spec/requests/inventory_foods_spec.rb
+++ spec/requests/inventory_foods_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "InventoryFoods", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'testing request' do
+    it "GET /inventories/:id/inventory_foods/new" do
+      user = User.create!(name: 'Dan')
+      inventory = user.inventories.create!(name: "Inv#1")
+      get("/inventories/#{inventory.id}/inventory_foods/new")
+      expect(response).to render_template('new')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "POST /inventories/:id/inventory_foods" do
+      user = User.create!(name: 'Dan')
+      inventory = user.inventories.create!(name: "Inv#1")
+      food = user.foods.create!(name: 'Apple', measurement_unit: 'grams', price: 321)
+      post("/inventories/#{inventory.id}/inventory_foods", params: { inventory_food: { inventory_id: inventory.id, food_id: food.id, quantity: 5.5 } })
+      expect(response).to redirect_to("/inventories/#{inventory.id}")
+      delete("/inventory_foods/#{InventoryFood.last.id}")
+      expect(response).to redirect_to("/inventories/#{inventory.id}")
+    end
   end
 end
