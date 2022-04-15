f55c1b7bf0f5ae83da81e5000fcde91471b6ffa2
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-15 11:15:04 +0200

    Create rspec for shopping list controller

--- spec/requests/shopping_list_spec.rb
+++ spec/requests/shopping_list_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "ShoppingLists", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'testing request' do
+    it "GET /public_recipes" do
+      user = User.create!(name: 'Dan')
+      inventory = user.inventories.create!(name: "Inv#1")
+      recipe = user.recipes.create!(name: 'Chopped apples', preparation_time: 1, cooking_time: 2, description: 'In order to cook...')
+      get("/shopping_list", params: { inventory_id: inventory.id, recipe_id: recipe.id })
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
